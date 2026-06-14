872de3ad7d890b3ef5eb3a079581efb8f69a7014
WCG847 <wcg847@users.noreply.github.com>
2026-06-14 20:57:15 +0100

    fixed

--- Modules/Private/Implementations/File.cpp
+++ Modules/Private/Implementations/File.cpp
@@ -2,14 +2,16 @@ module;
 #include <windows.h>
 module AssortX:IO;
 import :Types;
+import :String;
 
 namespace AssortX::IO {
 	File FileManager::Open(const char16_t *&Name) {
 		HANDLE hfile = CreateFileW(reinterpret_cast<const wchar_t *>(Name),
-					GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, nullptr,
+					GENERIC_READ | GENERIC_WRITE | GENERIC_EXECUTE, FILE_SHARE_READ, nullptr,
 					OPEN_EXISTING, 0x80, nullptr);
 		HANDLE hmem = CreateFileMappingW(hfile, nullptr, PAGE_EXECUTE_READWRITE, 0, 0,
-						   reinterpret_cast<const wchar_t *>(Name));
+						   nullptr);
+
 		void *mem = MapViewOfFile(hmem, FILE_MAP_READ | FILE_MAP_EXECUTE | FILE_MAP_WRITE,
 					  0, 0, 0);
 		CloseHandle(hmem);
--- Modules/Private/Implementations/Startup.cpp
+++ Modules/Private/Implementations/Startup.cpp
@@ -1,4 +1,6 @@
 import AssortX;
+extern "C" __declspec(dllimport) int NtTerminateProcess(void *, int);
+
 
 extern "C" __declspec(dllimport) const char16_t **
 	CommandLineToArgvW(const char16_t *lpCmdLine, int *pNumArgs);
@@ -7,15 +9,14 @@ extern "C" __declspec(dllimport) const char16_t *GetCommandLineW();
 extern "C" __declspec(dllimport) void *LocalFree(void *hMem);
 
 extern "C" long long Startup(int argc, const char16_t *argv[]);
-extern "C" long long AssortX_Startup() {
+extern "C" void AssortX_Startup() {
 	AssortX::String::StringUtils::DebugPrint(
 		u"%ls v%d.%.02d\r\n", AssortX::Build::Information.ProductName,
 		AssortX::Build::Information.Version.Major,
 		AssortX::Build::Information.Version.Minor);
 	int nArgs;
 	const char16_t **argv = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-	
 	long long Result = Startup(nArgs, argv);
 	LocalFree(argv);
-	return Result;
+	NtTerminateProcess(reinterpret_cast<void *>(-1), static_cast<int>(Result));
 }
